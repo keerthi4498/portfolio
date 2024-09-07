9cf8fcb3a631d51a2c978324d77d38460f483bf9
keerthanarajendran4498 <keerthana44@outlook.com>
2024-09-08 00:25:52 +0530

    menu-function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,29 @@
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
+  // Your custom options
+});
+
+var myOffcanvas = document.getElementById('menuoffcanvas')
+var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
+  
+document.querySelectorAll('.mobile-menu li').forEach(item => {
+  item.addEventListener('click', () => {
+    bsOffcanvas.hide();
   });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      const headerOffset = 80;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
+      });
+  });
+});
