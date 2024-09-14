9e6be460c88a82554c8f9007ac50f9d5363ee378
keerthanarajendran4498 <keerthana44@outlook.com>
2024-09-14 16:35:14 +0530

    top to scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,41 @@
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
-
-var myOffcanvas = document.getElementById('menuoffcanvas')
-var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
-  
-document.querySelectorAll('.mobile-menu li').forEach(item => {
-  item.addEventListener('click', () => {
-    bsOffcanvas.hide();
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    "use strict";
+    //Scroll back to top
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
   });
-});
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-
-      const headerOffset = 80;
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-
-      window.scrollTo({
-          top: offsetPosition,
-          behavior: "smooth"
-      });
-  });
-});
+})(jQuery);
